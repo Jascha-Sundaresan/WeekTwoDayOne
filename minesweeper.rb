d1b32b72f69e9b48ffcfbcfa41cafedc6e7adcf9
Jascha-Sundaresan <flizarthanon@gmail.com>
2014-10-13 11:32:21 -0700

    Build *working* board and tiles

--- minesweeper.rb
+++ minesweeper.rb
@@ -4,36 +4,43 @@ class Board
   end
   
   def build_board
-    new_board = Array.new(9, Array.new(9,nil))
-    9.times do |row|
-      9.times do |tile|
-        new_board[row][tile] = Tile.new
-      end
-    end
+    new_board = Array.new(9) { Array.new(9) { Tile.new } }
+    # 9.times do |row|
+    #   9.times do |tile|
+    #     new_board[row][tile] = Tile.new
+    #   end
+    # end
     dispence_mines(new_board)
   end
   
   def dispence_mines(new_board)
-    total_mines = 0
-    until total_mines == 9
-      x = rand(9)
-      y = rand(9)
-      current_tile = new_board[x][y]
-      unless current_tile.mined?
-        # current_tile.mined = true
-        current_tile.place_mine
-        total_mines +=1
-      end
-    end
+    new_board.flatten.sample(9).each { |tile| tile.place_mine }
+    # total_mines = 0
+    # until total_mines == 9
+    #   x = rand(9)
+    #   p x
+    #   y = rand(9)
+    #   p y
+    #   current_tile = new_board[x][y]
+    #   p current_tile
+    #   unless current_tile.mined?
+    #     current_tile.place_mine
+    #     total_mines +=1
+    #   end
+    # end
+    # p total_mines
     new_board
+  end
+  
+  def display
+    p @current_board
   end
   
   
 end
 
 class Tile
-  # attr_accessor :mined
-  
+    
   def initialize
     @mined = false
   end
@@ -49,7 +56,12 @@ class Tile
 end
 
 class Game
+  attr_reader :board
+  
   def initialize
     @board = Board.new
   end
 end
+
+
+Game.new.board.display
